24da57dc5b90af78bc3989bdf496578f55aeeac0
Rostyslav Shymchak <rostyslav.shymchak@jetsoftpro.com>
2021-03-18 10:17:46 +0200

    Init

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
-street light recognition
+# Street Light Luminance Detection
+A Flask Web-App detect object in image and calculate Luminosity for the street lights detected
+
+
+## Project Structure
+
+__app.py__ : To deploy the Flask service
+
+__inference.py__ : Script to load the object detection yolo model and infer the image
+
+__cal_lum.py__ : Script to calculate the luminosity of the given image
+
+### Local Environment Instructions
+
+1. Download and extract the given folder
+
+2. Install a few required pip packages, which are specified in the requirements text file (including OpenCV).
+    ```
+    pip install -r requirements.txt
+    ```
+3. Darknet needs to be setup (https://github.com/AlexeyAB/darknet.git)
+
+4. Run the app.py
+    ```
+    python app.py
+    ```
+
+    wait to a few seconds the darknet yolo model will load and it will display an IP address (http://127.0.0.1:5000/)
+
+5. Open the URL in browser and its good to go
---
+++ app.py
@@ -0,0 +1,63 @@
+import os
+# from app import app
+import urllib.request
+from flask import Flask, flash, request, redirect, url_for, render_template, jsonify
+from werkzeug.utils import secure_filename
+
+from inference import load_model, get_result
+
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
+
+UPLOAD_FOLDER = 'static/uploads/'
+
+app = Flask(__name__)
+app.secret_key = "secret key"
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+filename= ''
+
+netMain, metaMain= load_model()
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/')
+def upload_form():
+    return render_template('upload.html')
+    
+@app.route('/', methods=['POST'])
+def upload_image():
+    if 'file' not in request.files:
+        flash('No file part')
+        return redirect(request.url)
+
+    file = request.files['file']
+    if file.filename == '':
+        flash('No image selected for uploading')
+        return redirect(request.url)
+
+    if file and allowed_file(file.filename):
+        global filename
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+
+        return render_template('upload.html', filename=filename)
+    else:
+        flash('Allowed image types are -> png, jpg, jpeg')
+        return redirect(request.url)
+
+@app.route('/display/<filename>')
+def display_image(filename):
+    print('display_image filename: ' + filename)
+    return redirect(url_for('static', filename='uploads/' + filename), code=301)
+
+@app.route('/get_prediction')
+def get_prediction():
+  
+  imagePath= os.path.join(UPLOAD_FOLDER, filename)
+  img, lum= get_result(imagePath, netMain, metaMain)
+  return jsonify({'luminosity': lum, 'img': img})
+
+
+if __name__ == "__main__":
+    app.run(debug= True)   
